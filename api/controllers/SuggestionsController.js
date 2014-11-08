d01f86f1fdb4594006cce7fe659f7eed23131099
David Maitland <hello@davidmaitland.me>
2014-11-08 14:47:52 +0000

    Added Suggestions model and controller

---
+++ api/controllers/SuggestionsController.js
@@ -0,0 +1,23 @@
+/**
+ * SuggestionsController
+ *
+ * @description :: Server-side logic for managing suggestions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+    suggestion: {
+      type: 'STRING'
+    },
+
+    details: {
+      type: 'STRING'
+    },
+
+    votes: {
+      type: 'INTEGER'
+    }
+	
+};
+
