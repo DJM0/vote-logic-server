34e11c359f9fd3006de325cc65d71d0a3b29a003
David Maitland <david.maitland@sipcentric.com>
2014-12-12 13:26:43 +0000

    Testing passport.js

--- config/routes.js
+++ config/routes.js
@@ -46,8 +46,14 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  '/api/suggestions': {
-    populate_votes_limit: 5
-  }
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback',
+
+  'get /auth/:provider': 'AuthController.provider',
+  'get /auth/:provider/callback': 'AuthController.callback'
 
 };
